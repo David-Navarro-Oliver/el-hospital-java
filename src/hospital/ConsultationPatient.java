b8926d3dcc5c90f62daefff8f0297b14a3e53d2b
David-Navarro-Oliver <navarropalle@gmail.com>
2026-03-03 00:17:41 +0100

    feat(patient): add ConsultationPatient implementation

--- src/hospital/ConsultationPatient.java
+++ src/hospital/ConsultationPatient.java
@@ -1,4 +1,45 @@
 package hospital;
 
-public class ConsultationPatient {
+public class ConsultationPatient extends Patient {
+
+    public ConsultationPatient(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public void admission() {
+        System.out.println(getName() + " (Consulta) -> Admisión");
+    }
+
+    public void nurseControl() {
+        System.out.println(getName() + " (Consulta) -> Control de enfermería");
+    }
+
+    @Override
+    public void room() {
+        System.out.println(getName() + " (Consulta) -> Habitación");
+    }
+
+    @Override
+    public void treatment() {
+        System.out.println(getName() + " (Consulta) -> Tratamiento");
+    }
+
+    public void nextAppointment() {
+        System.out.println(getName() + " (Consulta) -> Próxima cita");
+    }
+
+    @Override
+    public void discharge() {
+        System.out.println(getName() + " (Consulta) -> Salida");
+    }
+
+    @Override
+    public String toString() {
+        return "ConsultationPatient{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                '}';
+    }
 }
